79843bf41d1bde1c8fbf272737e8a348af0fc47b
Benito <benitopedro21@outlook.com>
2024-02-25 17:00:04 -0300

    🐛 FIX: Dependency Injection

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampaignResolver } from './campaigns.resolver';
 import { Campaign } from 'src/graphql/models/Campaign';
 import { CampaignService } from './campaigns.service';
+import { User } from 'src/graphql/models/User';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign])],
+  imports: [TypeOrmModule.forFeature([Campaign, User])],
   providers: [CampaignResolver, CampaignService],
   exports: [CampaignService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,10 @@ import { UserResolver } from './users.resolver';
 import { UserService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/graphql/models/User';
-import { Campaign } from 'src/graphql/models/Campaign';
-import { CampaignService } from 'src/campaigns/campaigns.service';
+import { CampaignModule } from 'src/campaigns/campaigns.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Campaign])],
-  providers: [UserResolver, UserService, CampaignService],
+  imports: [TypeOrmModule.forFeature([User]), CampaignModule],
+  providers: [UserResolver, UserService],
 })
 export class UsersModule {}
